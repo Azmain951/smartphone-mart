7ff9c93823e526bdd172b4bed60dd4a1d23f74dc
Afridi Azmain <Azmain951>
2022-02-28 20:46:27 +0600

    displaying phones on UI

--- js/phone.js
+++ js/phone.js
@@ -5,5 +5,24 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displayPhone(data.data));
 };
+
+const displayPhone = phones => {
+    const phoneContainer = document.getElementById('phones');
+
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(div);
+    });
+}
