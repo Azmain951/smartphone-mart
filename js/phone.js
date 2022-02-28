cf75fd484efab5b4c7140101cc5baea85b568d1b
Afridi Azmain <Azmain951>
2022-02-28 21:30:35 +0600

    phone details section added

--- js/phone.js
+++ js/phone.js
@@ -6,23 +6,67 @@ const searchPhone = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data));
+
+    searchField.value = '';
 };
 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('phones');
-
+    phoneContainer.textContent = '';
+    let count = 0;
+    console.log(phones.length)
     phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+        if (count < 20) {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
             <div class="card">
-                <img src="${phone.image}" class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">${phone.brand}</p>
+                    <a onclick ="getPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</a>
                 </div>
             </div>
         `;
-        phoneContainer.appendChild(div);
+            phoneContainer.appendChild(div);
+            count++;
+        }
+        else {
+            return 0;
+        }
     });
-}
+};
+
+const getPhoneDetails = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data));
+};
+
+const displayPhoneDetails = phone => {
+    const phoneDetailsContainer = document.getElementById('phone-details');
+    phoneDetailsContainer.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('row');
+    div.innerHTML = `
+                <div class="col-md-4 d-flex justify-content-center align-items-center">
+                    <img src="${phone.image}" class="img-fluid w-100 p-4" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body p-4">
+                        <h5 class="card-title">${phone.name}</h5>
+                        <p class="card-text">${phone.releaseDate}</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item ps-0">${phone.mainFeatures.storage}</li>
+                            <li class="list-group-item ps-0">${phone.mainFeatures.displaySize}</li>
+                            <li class="list-group-item ps-0">${phone.mainFeatures.chipSet}</li>
+                            <li class="list-group-item ps-0">${phone.mainFeatures.memory}</li>
+                        </ul>
+                    </div>
+                </div>
+    `;
+    phoneDetailsContainer.appendChild(div);
+};
+
